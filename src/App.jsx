08cb2aa55fb6b4804b5443ffe47e221f9499f1a6
semyonsurkov <semyonsurkov@outlook.com>
2023-08-27 03:43:49 +0300

    Add new API

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 export default function App() {
@@ -13,28 +13,38 @@ function Player() {
   const [player, setPlayer] = useState('');
   const [img, setImg] = useState('');
 
-  async function fetchRandomPlayer() {
+  async function fetchPlayerData() {
     try {
-      const response = await fetch('http://31.129.102.114:5000/players');
+      const response = await fetch(
+        'https://my-json-server.typicode.com/semyonsurkov/my-api/players'
+      );
       const data = await response.json();
 
-      const randomIndex = Math.floor(Math.random() * data.length);
-      const randomPlayer = data[randomIndex].Name;
-      const randomImg = data[randomIndex].img;
+      if (data && data.length > 0) {
+        const randomIndex = Math.floor(Math.random() * data.length);
+        const randomPlayer = data[randomIndex].Name;
+        const randomImg = data[randomIndex].img;
 
-      setPlayer(randomPlayer);
-      setImg(randomImg);
+        setPlayer(randomPlayer);
+        setImg(randomImg);
+      } else {
+        console.error('No player data available.');
+      }
     } catch (error) {
       console.error('Error fetching player:', error);
     }
   }
+
+  useEffect(() => {
+    fetchPlayerData();
+  }, []); // Fetch data when the component mounts
 
   return (
     <div className="player">
       <h1>Random Player Generator</h1>
       <p>{player}</p>
       <img src={img} alt="Player" className="player-image" />
-      <button onClick={fetchRandomPlayer}>Get the new player</button>
+      <button onClick={fetchPlayerData}>Get the new player</button>
     </div>
   );
 }
